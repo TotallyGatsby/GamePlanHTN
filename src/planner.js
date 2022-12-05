6c372d6eec02bf7cdc66b7f50545d907c86d105f
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2022-12-04 21:31:25 -0800

    Add FluidHTN copyright notice to the planner file

--- src/planner.js
+++ src/planner.js
@@ -1,3 +1,7 @@
+// Portions of this file are derived from FluidHTN (MIT License)
+// Copyright (c) 2019 Pål Trefall
+// https://github.com/ptrefall/fluid-hierarchical-task-network
+
 import PrimitiveTask from "./Tasks/primitiveTask";
 import TaskStatus from "./taskStatus";
 import DecompositionStatus from "./decompositionStatus";
